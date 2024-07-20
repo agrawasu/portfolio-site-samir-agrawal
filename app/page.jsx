2bc0a2093ad0c1e625098c6c435a34647783fb00
agrawasu <agrawasu@mail.uc.edu>
2024-07-19 23:09:33 -0400

    fix bug

--- app/page.jsx
+++ app/page.jsx
@@ -17,7 +17,7 @@ const Home = () => {
           <div className="text-center xl:text-left order-2 xl:order-none">
             <span className="text-xl">Expertise</span>
             <h1 className="h1 mb-6">
-              Hello I'm <br /> <span className="text-accent">Name</span>
+              Hello I am&apos; <br /> <span className="text-accent">Name</span>
             </h1>
             <p className="max-w-[500px] mb-9 text-white/80">{filler}</p>
 
