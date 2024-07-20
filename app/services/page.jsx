72378f66141b0b2079e640e0ab3959fe28b5da66
agrawasu <agrawasu@mail.uc.edu>
2024-07-20 10:11:59 -0400

    finish services page with styling

--- app/services/page.jsx
+++ app/services/page.jsx
@@ -1,82 +1,95 @@
 "use client";
 
-import { BsArrowDownRight } from 'react-icons/bs'
-import Link from 'next/link'
+import { BsArrowDownRight } from "react-icons/bs";
+import Link from "next/link";
 
 const service = [
   {
-    num: '01',
-    titles: 'Service 01',
-    description: "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ad blanditiis sint, numquam possimus suscipit ducimus.",
-    href: '',
+    num: "01",
+    title: "Service 01",
+    description:
+      "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ad blanditiis sint, numquam possimus suscipit ducimus.",
+    href: "",
   },
   {
-    num: '02',
-    titles: 'Service 02',
-    description: "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ad blanditiis sint, numquam possimus suscipit ducimus.",
-    href: '',
+    num: "02",
+    title: "Service 02",
+    description:
+      "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ad blanditiis sint, numquam possimus suscipit ducimus.",
+    href: "",
   },
   {
-    num: '03',
-    titles: 'Service 03',
-    description: "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ad blanditiis sint, numquam possimus suscipit ducimus.",
-    href: '',
+    num: "03",
+    title: "Service 03",
+    description:
+      "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ad blanditiis sint, numquam possimus suscipit ducimus.",
+    href: "",
   },
   {
-    num: '04',
-    titles: 'Service 04',
-    description: "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ad blanditiis sint, numquam possimus suscipit ducimus.",
-    href: '',
+    num: "04",
+    title: "Service 04",
+    description:
+      "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ad blanditiis sint, numquam possimus suscipit ducimus.",
+    href: "",
   },
-]
+];
 
-import { motion } from 'framer-motion';
+import { motion } from "framer-motion";
 
 const Services = () => {
   return (
-    <section className='min-h-[80vh] flex flex-col justify-center py-12 xl:py-0'>
+    <section className="min-h-[80vh] flex flex-col justify-center py-12 xl:py-0">
       <div className="container mx-auto">
         <motion.div
-          initial={{opacity: 0}}
+          initial={{ opacity: 0 }}
           animate={{
             opacity: 1,
-            transition:{
+            transition: {
               delay: 2.4,
               duration: 0.4,
-              ease: "easeIn"
-              },
-            }}
-            className="grid grid-cols-1 md:grid-cols-2 gap-[60px]"
-          >
-            {service.map((service, index) => {
-              return (
-                <div className="flex-1 flex flex-col justify-center gap-6 group" key={index}>
-
-                  {/* top */}
-                  <div className="w-full flex justify-between items-center">
-                    <div className="text-5xl font-extrabold text-outline text-transparent group-hover:text-outline-hover">
-                      {service.num}
-                    </div>
-                    <Link href={service.href}>
-                      <BsArrowDownRight />
-                    </Link>
+              ease: "easeIn",
+            },
+          }}
+          className="grid grid-cols-1 md:grid-cols-2 gap-[60px]"
+        >
+          {service.map((service, index) => {
+            return (
+              <div
+                className="flex-1 flex flex-col justify-center gap-6 group"
+                key={index}
+              >
+                {/* top */}
+                <div className="w-full flex justify-between items-center">
+                  <div className="text-5xl font-extrabold text-outline text-transparent group-hover:text-outline-hover transition-all duration-500">
+                    {service.num}
                   </div>
-
-                  {/* title */}
-                  <h2>{service.title}</h2>
-
-                  {/* description */}
-                  <p>{service.description}</p>
-
-                  {/* border */}
-                  <div className="border-b border-white/20 w-full"></div>
+                  <Link
+                    href={service.href}
+                    className="w-[70px] h-[70px] rounded-full bg-white group-hover:bg-accent transition-all duration-500 flex justify-center items-center hover:-rotate-45"
+                  >
+                    <BsArrowDownRight className="text-primary text-3xl" />
+                  </Link>
                 </div>
-              );
-            })}
+
+                {/* title */}
+                <h2 className="text-[42px] font-bold leading-none text-white group-hover:text-accent transition-all duration-500">
+                  {service.title}
+                </h2>
+
+                {/* description */}
+                <p className="text-white/50 group-hover:text-white/80 transition-all duration-500">
+                  {service.description}
+                </p>
+
+                {/* border */}
+                <div className="border-b border-white/20 w-full"></div>
+              </div>
+            );
+          })}
         </motion.div>
       </div>
     </section>
-  )
-}
+  );
+};
 
-export default Services
+export default Services;
