63276577c45d0c28537aaabb6f7d7f29849176be
agrawasu <agrawasu@mail.uc.edu>
2024-07-19 20:12:55 -0400

    initialize homepage and begin services page

--- components/Header.jsx
+++ components/Header.jsx
@@ -1,15 +1,31 @@
 import Link from "next/link"
 import { Button } from "./ui/button"
 
+import Nav from "./Nav"
+
 const Header = () => {
   return (
     <header className="py-8 xl:py text-white">
-        <div className="container mx-auto">
+        <div className="container mx-auto flex justify-between items-center">
+            {/* logo */}
             <Link href="/">
                 <h1 className="text-4xl font-semibold">
                     Samir<span className="text-accent">.</span>
                 </h1>
             </Link>
+
+
+            {/* desktop nav & hire me */}
+            <div className="hidden xl:flex items-center gap-8">
+                <Nav />
+                <Link href="/contact">
+                    <Button>Hire me</Button>
+                </Link>
+            </div>
+
+            {/* mobile nav */}
+            <div className="xl:hidden">mobile nav</div>
+
         </div>
     </header>
   )
