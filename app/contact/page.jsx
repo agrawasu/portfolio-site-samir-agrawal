6b2e974478959d1907f4375d28268ce51be2f1ab
agrawasu <agrawasu@mail.uc.edu>
2024-07-21 14:59:35 -0400

    minor bug fixes

--- app/contact/page.jsx
+++ app/contact/page.jsx
@@ -16,6 +16,7 @@ import {
 
 import { FaPhoneAlt, FaEnvelope, FaMapMarkerAlt } from "react-icons/fa";
 
+const contactMessage = "Let's Work Together";
 const info = [
   {
     icon: <FaPhoneAlt />,
@@ -79,7 +80,7 @@ const Contact = () => {
               className="flex flex-col gap-6 p-10 bg-secondary rounded-xl"
               onSubmit={handleSubmit}
             >
-              <h3 className="text-4xl text-accent">Let's work together</h3>
+              <h3 className="text-4xl text-accent">{contactMessage}</h3>
               <p className="text-white/60">
                 Lorem ipsum dolor sit amet consectetur adipisicing elit. Delectus eos iste
                 neque ipsum totam provident cumque, eaque, odit at, sint vel omnis illo
