368750bf055fbae844ad4f6e5d0977ef7fbf9be0
agrawasu <agrawasu@mail.uc.edu>
2024-07-23 20:39:19 -0400

    minor style changes

--- app/layout.jsx
+++ app/layout.jsx
@@ -20,6 +20,9 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      <head>
+        <link rel="icon" href="/public/favicon.ico" />
+      </head>
       <body className={jetbrainsMono.variable}>
         <Header />
         <StairTransition />
