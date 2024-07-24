e347120fcc9d418590711fb7e0960432e179b156
agrawasu <agrawasu@mail.uc.edu>
2024-07-23 20:43:24 -0400

    favicon fixes

--- app/layout.jsx
+++ app/layout.jsx
@@ -21,7 +21,7 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <head>
-        <link rel="icon" href="/public/favicon.ico" />
+        <link rel="icon" href="/favicon.ico" />
       </head>
       <body className={jetbrainsMono.variable}>
         <Header />
